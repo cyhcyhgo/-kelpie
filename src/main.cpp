e0e338d34c1bc32e5599bbad3ee7a4030d273d8f
程英昊 <974016477@qq.com>
2024-09-12 23:17:32 +0800

    test1

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);  // Teensy 3.2 自带的 LED
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // 点亮 LED
+  delay(1000);                      // 保持 1 秒
+  digitalWrite(LED_BUILTIN, LOW);   // 熄灭 LED
+  delay(1000);                      // 保持 1 秒
+}
